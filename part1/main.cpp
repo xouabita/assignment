706988260c8977ea7dd86af3c88452f3232121c7
xouabita <xouabita@gmail.com>
2014-08-08 16:44:07 +0200

    add sort method for sorting list

--- part1/main.cpp
+++ part1/main.cpp
@@ -109,6 +109,7 @@ template <class T> class List {
   // Return the size of the list
   int getSize() { return size; }
 
+  // Return a sub list
   List sub (int index, int length) {
     if (index < 0 || index >= size) throw "Index is out of bounds";
     if (length <= 0 || index+length > size) throw "Length is invalid";
@@ -118,6 +119,46 @@ template <class T> class List {
       res.push_back((*this)[i]);
     }
     return res;
+  }
+
+  void sort() {
+    if (size > 2) {
+      int demi_size = size/2;
+      List part1 = this->sub(0,(demi_size));
+      List part2 = this->sub(demi_size,size-demi_size);
+      part1.sort(); part2.sort();
+
+      // Apply Merge Sort
+      Node<T> * a  = head;
+      Node<T> * a1 = part1.head;
+      Node<T> * a2 = part2.head;
+
+      while (a && a1 && a2) {
+        if (a1->data <= a2->data) {
+          a->data = a1->data;
+          a1 = a1->next;
+        } else {
+          a->data = a2->data;
+          a2 = a2->next;
+        }
+        a = a->next;
+      }
+
+      if (!a1) a1 = a2;
+
+      while (a1 && a) {
+        a->data = a1->data;
+        a = a->next;
+        a1 = a1->next;
+      }
+
+    } else if (size == 2) {
+      if (head->data > head->next->data) {
+        T temp = head->data;
+        head->data = head->next->data;
+        head->next->data = temp;
+      }
+    }
   }
 
   // Display list into ostream
@@ -137,22 +178,22 @@ template <class T> class List {
   T operator [] (int i) {
     return getNode(i)->data;
   }
-
 };
 
 int main () {
-  List<int> l ({2,7,8,9,2,6,10,67,23});
-  cout << l << endl;
+  List<int> l1 ({9,8,7,6,5,4,3,2,1,0});
+  List<int> l2 ({1,2,3,4,5,6,7,8,9,10,11});
+  List<int> l3 ({7,89,2,67,2,78,10010,2,-10,78});
 
-  List<int> sl1 = l.sub(0,2); // Expect 2 -> 7
-  List<int> sl2 = l.sub(2,4); // Expect 8 -> 9 -> 2 -> 6
+  cout << l1 << endl;
+  l1.sort();
+  cout << l1 << endl << endl;
 
-  cout << sl1 << endl;
-  cout << sl2 << endl;
+  cout << l2 << endl;
+  l2.sort();
+  cout << l2 << endl << endl;
 
-  List<int> l_part1 = l.sub(0,(l.getSize()/2));
-  List<int> l_part2 = l.sub((l.getSize()/2),(l.getSize()-(l.getSize()/2)));
-
-  cout << "part 1: " << l_part1 << endl;
-  cout << "part 2: " << l_part2 << endl;
+  cout << l3 << endl;
+  l3.sort();
+  cout << l3 << endl << endl;
 }
