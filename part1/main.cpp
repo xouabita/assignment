058105e30f3637588092b767e97ad6d38bf9e06d
xouabita <xouabita@gmail.com>
2014-08-08 14:12:56 +0200

    add comments and change name of queue by tail

--- part1/main.cpp
+++ part1/main.cpp
@@ -3,6 +3,7 @@
 
 using namespace std;
 
+// Node into a linked list
 template <class T> struct Node {
   T data;
   Node * next;
@@ -12,11 +13,13 @@ template <class T> struct Node {
   }
 };
 
+// Linked List
 template <class T> class List {
   int size;
   Node<T> * head;
-  Node<T> * queue;
+  Node<T> * tail; // Tail to access the last element of the list
 
+  // Return a node at the specified index
   Node<T> * getNode (int i) {
     Node<T> * res = NULL;
     if (i < size && i >= 0) {
@@ -29,12 +32,14 @@ template <class T> class List {
     return res;
   }
 
+  // copy a list
   void cp (List & l) {
     for (int i=0; i < l.getSize(); i++) {
       this->push_back(l[i]);
     }
   }
 
+  // Free the memory
   void del() {
     Node<T> * n;
     while (head != NULL) {
@@ -42,69 +47,80 @@ template <class T> class List {
       head = head->next;
       delete n;
     }
-    head = queue = NULL;
+    head = tail = NULL;
     size = 0;
   }
 
   public:
-  List (): size (0), head (NULL), queue (NULL) {}
+  // Initialize an empty list
+  List (): size (0), head (NULL), tail (NULL) {}
 
-  List (List & l): size (0), head (NULL), queue (NULL) {
+  // Copy initialization with deep copy
+  List (List & l): size (0), head (NULL), tail (NULL) {
     cp (l);
   }
 
-  List (std::initializer_list<T> args): size (0), head (NULL), queue (NULL) {
+  // Initialize a list with {} list elements
+  List (std::initializer_list<T> args): size (0), head (NULL), tail (NULL) {
     for (auto item: args) this->push_back(item);
   }
 
+  // Assign operator
   List operator= (List & l) {
     this->del();
-    head = queue = NULL;
+    head = tail = NULL;
     size = 0;
     cp (l);
     return *this;
   }
 
+  // Deletion of the List
   ~List () {
     del();
   }
 
+  // Push an element at the front of the list
   void push_front (T e) {
     head = new Node<T> (e, head);
-    if (queue == NULL) {
-      queue = head;
+    if (tail == NULL) {
+      tail = head;
     }
     size++;
   }
 
+  // Push an element at the back of the list
   void push_back (T e) {
-    if (queue == NULL) {
-      head = queue = new Node<T> (e);
+    if (tail == NULL) {
+      head = tail = new Node<T> (e);
     }
     else {
-      queue->next = new Node<T> (e);
-      queue = queue->next;
+      tail->next = new Node<T> (e);
+      tail = tail->next;
     }
     size++;
   }
 
+  // Return the size of the list
   int getSize() { return size; }
 
+  // Display list into ostream
   friend ostream & operator << (ostream & os, List l) {
     if (l.head != NULL) {
       Node<T> * n = l.head;
-      while (n != l.queue) {
+      while (n != l.tail) {
        os << n->data << " -> ";
        n = n->next;
       }
-      os << l.queue->data;
+      os << l.tail->data;
     } else { os << "Empty List."; }
     return os;
   }
 
+  // Get an element in the list as for an array
   T operator [] (int i) {
     return getNode(i)->data;
   }
+
 };
 
 int main () {
