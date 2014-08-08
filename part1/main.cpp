e5e64209a1ff5035479c193a9d7acff00ba99529
xouabita <xouabita@gmail.com>
2014-08-08 22:32:28 +0200

    add the intersection method

--- part1/main.cpp
+++ part1/main.cpp
@@ -3,6 +3,11 @@
 
 using namespace std;
 
+
+/* ===================================================================
+ * =                    Classes for the task                         =
+ * =================================================================== */
+
 // Node into a linked list
 template <class T> struct Node {
   T data;
@@ -121,6 +126,7 @@ template <class T> class List {
     return res;
   }
 
+  // Use Merge Sort for sorting the list
   void sort() {
     if (size > 2) {
       int demi_size = size/2;
@@ -180,20 +186,71 @@ template <class T> class List {
   }
 };
 
-int main () {
-  List<int> l1 ({9,8,7,6,5,4,3,2,1,0});
-  List<int> l2 ({1,2,3,4,5,6,7,8,9,10,11});
-  List<int> l3 ({7,89,2,67,2,78,10010,2,-10,78});
+/* ===================================================================
+ * =                           Solution                              =
+ * ===================================================================
+ * =                                                                 =
+ * = I used the merge sort algorithm to sort the two lists and then  =
+ * = I compare the two lists.                                        =
+ * =                                                                 =
+ * ===================================================================
+ */
 
-  cout << l1 << endl;
+
+template<class T> List<T> intersection (List<T> l1, List<T> l2) {
   l1.sort();
-  cout << l1 << endl << endl;
-
-  cout << l2 << endl;
   l2.sort();
-  cout << l2 << endl << endl;
 
-  cout << l3 << endl;
-  l3.sort();
-  cout << l3 << endl << endl;
+  List<T> res;
+  int i = 0, j = 0;
+  while (i < l1.getSize() && j < l2.getSize()) {
+    if (l1[i] == l2[j]) {
+      res.push_back(l1[i]);
+      i++; j++;
+    }
+    else if (l1[i] < l2[j]) i++;
+    else j++;
+  }
+
+  return res;
+}
+
+int main () {
+  // TESTS
+  List<int> l1 ({12,89,24,42,9,2,4,6,7});
+  List<int> l2 ({26,56,69,12,4,7,24,112,45,38});
+  List<int> l3 ({90,89,56,4,7,2,7,42,24});
+  List<int> l4 ({1});
+  List<int> l5 ({48,89,12});
+
+  List<int> l6 ({1,7,9,3,5});
+  List<int> l7 ({2,4,6,7,9});
+
+  cout << "intersection of:" << endl;
+  cout << "l1: " << l1 << endl;
+  cout << "l2: " << l2 << endl;
+  cout << "result: " << intersection(l1,l2) << endl << endl;
+
+
+
+
+  cout << "intersection of:" << endl;
+  cout << "l3: " << l3 << endl;
+  cout << "l4: " << l4 << endl;
+  cout << "result: " << intersection(l3,l4) << endl << endl;
+
+  cout << "intersection of:" << endl;
+  cout << "l6: " << l6 << endl;
+  cout << "l7: " << l7 << endl;
+  cout << "result: " << intersection(l6,l7) << endl << endl;
+
+  cout << "intersection of:" << endl;
+  cout << "l1: " << l1 << endl;
+  cout << "l3: " << l3 << endl;
+  cout << "result: " << intersection(l1,l3) << endl << endl;
+
+  cout << "intersection of:" << endl;
+  cout << "l4: " << l4 << endl;
+  cout << "l5: " << l5 << endl;
+  cout << "result: " << intersection(l4,l5) << endl << endl;
 }
