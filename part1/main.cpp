ec4713f24ae0441987765c04145e3130524d1cf7
xouabita <xouabita@gmail.com>
2014-08-08 12:19:53 +0200

    add simple linked list class

--- part1/main.cpp
+++ part1/main.cpp
@@ -11,8 +11,46 @@ template <class T> struct Node {
   }
 };
 
+template <class T> class List {
+  Node<T> * head;
+  Node<T> * queue;
+  int size;
 
+  public:
+  List () {
+    head = queue = NULL;
+    size = 0;
+  }
+
+  void push_front (T e) {
+    head = new Node<T> (e, head);
+    if (queue == NULL) {
+      queue = head;
+    }
+  }
+
+  friend ostream & operator << (ostream & os, List l) {
+    if (l.head != NULL) {
+      Node<T> * n = l.head;
+      while (n != l.queue) {
+       os << n->data << " -> ";
+       n = n->next;
+      }
+      os << l.queue->data;
+    } else { os << "Empty List."; }
+    return os;
+  }
+};
 
 int main () {
-  // TESTS HERE
+  List<int> l;
+  cout << l << endl;
+  l.push_front(8);
+  cout << l << endl;
+  l.push_front(6);
+  l.push_front(9);
+  l.push_front(3);
+  l.push_front(7);
+
+  cout << l << endl;
 }
